6454a720587322f3031b024249fd5fbc2ac819dd
Pedro Martín del Campo González <pedro.martindelcampo.gonzalez@gmail>
2021-09-29 21:20:56 -0700

    Color tiles

--- src/app/tile/tile.component.ts
+++ src/app/tile/tile.component.ts
@@ -17,6 +17,20 @@ export class TileComponent {
     return this.value ? 'X' : 'O';
   }
 
+  get color() {
+    if (this.value === undefined) {
+      return 'bg-gray-600';
+    }
+    return this.value ? 'bg-green-600' : 'bg-red-600';
+  }
+
+  get hover() {
+    if (this.value === undefined) {
+      return 'hover:bg-gray-400';
+    }
+    return this.value ? 'hover:bg-green-400' : 'hover:bg-red-400';
+  }
+
   selectTile() {
     this.select.emit();
   }
