671e3ed7fd79f983399e0d4800b370b88622eef8
Pedro Martín del Campo González <pedro.martindelcampo.gonzalez@gmail>
2021-10-02 18:36:19 -0700

    Detect game finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  firstTurn = Math.random() >= 0.5;
-
-  get player() {
-    return this.firstTurn ? 'X' : 'O';
-  }
-
-  get color() {
-    return this.firstTurn ? 'text-green-500' : 'text-red-500';
-  }
-
-  onSelect() {
-    this.firstTurn = !this.firstTurn;
-  }
 
 }
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,22 +1,73 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html'
 })
 export class BoardComponent {
-  @Input() firstTurn: boolean;
+  firstTurn = Math.random() >= 0.5;
 
-  @Output() select = new EventEmitter<void>();
-  
+  player(turn: boolean) {
+    return turn ? 'X' : 'O';
+  }
+
+  color(turn: boolean) {
+    return turn ? 'text-green-500' : 'text-red-500';
+  }
+
+  @Output() finish = new EventEmitter<boolean>();
+
   board = new Array<boolean | undefined>(9);
 
   selectTile(index: number) {
     if (this.board[index] !== undefined) {
-      // Tile already hasa value
+      // Tile already has a value
       return;
     }
     this.board[index] = this.firstTurn;
-    this.select.emit();
+    this.firstTurn = !this.firstTurn;
   }
+
+  check(turn: boolean, indices: number[]): boolean {
+    return indices.map(i => this.board[i])
+                  .every(value => value === turn);
+  }
+
+  checkTurn(turn: boolean) {
+    return (
+      // Check rows
+      this.check(turn, [0, 1, 2]) ||
+      this.check(turn, [3, 4, 5]) ||
+      this.check(turn, [6, 7, 8]) ||
+      // Check cols
+      this.check(turn, [0, 3, 6]) ||
+      this.check(turn, [1, 4, 7]) ||
+      this.check(turn, [2, 5, 8]) ||
+      // Check diagonals
+      this.check(turn, [0, 4, 8]) ||
+      this.check(turn, [2, 4, 6])
+    );
+  }
+
+  /**
+   * Get winner
+   *
+   * true: first player (X)
+   * false: second player (O)
+   * undefined: game not finished
+   *
+   * Emits finish event if winner is not undefined;
+   */
+  get winner() {
+    if (this.checkTurn(true)) {
+      this.finish.emit(true);
+      return true;
+    }
+    if (this.checkTurn(false)) {
+      this.finish.emit(false);
+      return false;
+    }
+    return undefined;
+  }
+
 }
